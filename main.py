349a9c80eddf29c414a1003231f6d734dd1ac91d
xNazim <73472282+xNazim@users.noreply.github.com>
2020-12-21 10:25:40 +0600

    Update main.py

--- main.py
+++ main.py
@@ -22,3 +22,32 @@ Input_text.place(x=30, y=100)
 Label(root, text="Translation", font='aharoni 20 bold', bg='cadetblue').place(x=720, y=60)  # noqa
 Output_text = Text(root, font='calibri 13', height=11, wrap=WORD, padx=5, pady=5, width=47)  # noqa
 Output_text.place(x=600, y=100)
+
+
+language = list(LANGUAGES.values())
+
+src_lang = ttk.Combobox(root, values=language, width=22)
+src_lang.place(x=20, y=60)
+src_lang.set('choose input language')
+
+dest_lang = ttk.Combobox(root, values=language, width=22)
+dest_lang.place(x=890, y=60)
+dest_lang.set('choose output language')
+
+
+# Define function
+
+def Translate():
+    translator = Translator()
+    translated = translator.translate(text=Input_text.get(1.0, END), src=src_lang.get(), dest=dest_lang.get())  # noqa
+
+    Output_text.delete(1.0, END)
+    Output_text.insert(END, translated.text)
+
+
+# Translate Button
+trans_btn = Button(root, text='Translate', font='arial 15 bold', pady=5, fg='white', command=Translate, bg='red', activebackground='orange red')  # noqa
+trans_btn.place(x=478, y=180)
+
+
+root.mainloop()
